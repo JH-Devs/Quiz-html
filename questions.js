47b41548249ba85c26c378a9475ed219bcffd963
JH-devs <jitka.horavova01@gmail.com>
2023-10-26 16:31:10 +0200

    commit

---
+++ questions.js
@@ -0,0 +1,112 @@
+let questions = [
+    {
+        numb: 1,
+        question:"Frází 'dělám frontend' programátor říká, že:",
+        answer:"C. Programuje uživatelské rozhraní webu.",
+        options: [
+            "A. Označuje začátky a konce řádků patřičnými symboly v programovacím jazyku PHP.",
+            "B. Pracuje na předběžném přístupu aplikace, těsně před jejím dokončením.",
+            "C. Programuje uživatelské rozhraní webu.",
+            "D. Pracuje výhradně v HTML a CSS."
+        ]
+    },
+    {
+        numb: 2,
+        question:"Výrazem „vychytávání bugů“ se myslí:",
+        answer:"D. Činnost, kdy programátoři vychytávají softwarové chyby, tzv. bugy a tedy zjišťují a napravují to, co nefunguje.",
+        options: [
+            "A. Situace, kdy programátoři refaktorují kód, aby restruktovali a tím vylepšili existující kód.",
+            "B. Proces specializovaných firem, které se zaměřují na kvalitu kódu v rámci kybernetické bezpečnosti. V tomto případě jde o hledání digitálních štěnic.",
+            "C. Činnost, v rámci které programátoři hledají vir.",
+            "D. Činnost, kdy programátoři vychytávají softwarové chyby, tzv. bugy a tedy zjišťují a napravují to, co nefunguje."
+        ]
+    },
+    {
+        numb: 3,
+        question:"Pojmem „knihovna“ se označuje:",
+        answer:"B. Soubor předpřipravených kódů, které programátor může využít pro rychlejsí a lepší vývoj. Příkladem jedné z populárních knihoven je React.",
+        options: [
+            "A. Slovník odborných pojmů týkajících se IT. Obsahuje veškerou známou terminologii z oblasti IT.",
+            "B. Soubor předpřipravených kódů, které programátor může využít pro rychlejsí a lepší vývoj. Příkladem jedné z populárních knihoven je React.",
+            "C. Knihovna, která souvisí se stránkováním paměti, což je metoda správy virtální paměti.",
+            "D. Nástroj, který umožňuje snadnější hnedánív rámci vývojářského kódu."
+        ]
+    },
+    {
+        numb: 4,
+        question:"Spojení „dáme to na ostrý“ znamená:",
+        answer:"D. Vytvořená verze aplikace bude připravená online pro uživatele, odteď ji mohou používat.",
+        options: [
+            "A. Vytvoření hrubého náčrtu projektu, který se teprve bude ladit.",
+            "B. Proces schvalování vyšším managementem.",
+            "C. Tvorbu softwaru v jazyku C++, kterému programátoři přezdívají chilli.",
+            "D. Vytvořená verze aplikace bude připravená online pro uživatele, odteď ji mohou používat."
+        ]
+    },
+    {
+        numb: 5,
+        question:"Výrazem “sandbox” se rozumí:",
+        answer:"A. Kontrolovaný prostor, ve kterém lze testovat aplikace.",
+        options: [
+            "A. Kontrolovaný prostor, ve kterém lze testovat aplikace.",
+            "B. Termín opět vychází z angličtiny. Sand v tomto smyslu znamená opískovat, tedy očistit od přebytečných věcí, které programátor/kodér již nebude potřebovat.",
+            "C. Na pískovišti v jazyce programátorů probíhá ta nejtitěrnější práce, která vyžaduje absolutní soustředěnost.",
+            "D. Pískoviště je důležitá část tvůrčího procesu. Jde víceméně o brainstorming, z něhož později vzejde myšlenka, které se bude následně celý tým stoprocentně věnovat."
+        ]
+    },
+    {
+        numb: 6,
+        question:"Výrazem „fixnout to“ se rozumí:",
+        answer:"B. Opravení chyb.",
+        options: [
+            "A. Zvýraznění problémů, na kterých je potřeba v budoucnu pracovat.",
+            "B. Opravení chyb.",
+            "C. Zakonzervování projektu ve fázi, ve které se zrovna nachází.",
+            "D. Stanovení podmínek, za kterých budou probíhat následující kroky."
+        ]
+    },
+    {
+        numb: 7,
+        question:"Tvrzení „běží mi to na lokále“, znamená:",
+        answer:"B. Programátor spouští program ze svého počítače, který právě používá ",
+        options: [
+            "A. Výsledný produkt je dostupný jenom v určitých regionech. ",
+            "B. Programátor spouští program ze svého počítače, který právě používá ",
+            "C. Programátor má přístup ke zdrojovému kódu pouze z konkrétní země.",
+            "D. Programátor spouští program ze vzdáleného úložiště."
+        ]
+    },
+    {
+        numb: 8,
+        question:"Co je v programátorském slangu „zdroják“?",
+        answer:"C. Zdroják, jinak též zdrojový kód, je zápis v konkrétním programovacím jazyce, uložený jaký prostý text.",
+        options: [
+            "A. Pokud mezi programátory zmíníte, že hledáte zdroják, hledáte nejčastěji prvotní příčinu problému.",
+            "B. Zdrojákem se myslí zdrojové materiály klienta, pro něhož programátoři vytváří kód/software. Jde o zadání, podle něhož programátor postupuje.",
+            "C. Zdroják, jinak též zdrojový kód, je zápis v konkrétním programovacím jazyce, uložený jaký prostý text.",
+            "D. Zdroják je v programátorském slangu kostra dříve použitého kódu, který je možné použít i při práci na jiném produktu a nepodléhá autorským právům."
+        ]
+    },
+    {
+        numb: 9,
+        question:"Když se programátor zeptá „na které seš větvi?“, co zjišťuje?",
+        answer:"B. Programátor se snaží zjistit, ve které verzi kódu (větvi) jeho kolega či kolegyně pracuje.",
+        options: [
+            "A. Kód je rozdělený na jednotlivé řádky/větve a programátor se tak ptá, který řádek dotyčný upravuje.",
+            "B. Programátor se snaží zjistit, ve které verzi kódu (větvi) jeho kolega či kolegyně pracuje.",
+            "C. Otázkou se programátor ptá, ve kterém oddělení zaměstnanec pracuje. Výraz pochází z anglického branch, což znamená konkrétní oddělení ve firmě.",
+            "D. Otázka „na které seš větvi“ je slangové označení situace, kdy vývoj nejde podle plánu."
+        ]
+    },
+    {
+        numb: 10,
+        question:"Když programátor „dělá backend“, co dělá?",
+        answer:"D. Programátor je typicky zodpovědný za ukládání a manipulaci dat.",
+        options: [
+            "A. Programátor se soustředí na zpracovávání zálohy dat.",
+            "B. Z důvodu chyby se programátor vrací k předchozí verzi softwaru.",
+            "C. Pojem vychází z označení tenisového úderu. V programátorském slangu se tak začíná částmi, které jsou pro programátora obtížnější.",
+            "D. Programátor je typicky zodpovědný za ukládání a manipulaci dat."
+        ]
+    },
+]
